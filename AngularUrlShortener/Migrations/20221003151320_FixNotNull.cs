8ae0c54edf5a1577ccc6ec2abc01808039cf1336
dinomar <dinoalcatel@gmail.com>
2022-10-03 17:14:11 +0200

    Fix Db NotNull models

---
+++ AngularUrlShortener/Migrations/20221003151320_FixNotNull.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AngularUrlShortener.Migrations
+{
+    public partial class FixNotNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Hostname",
+                table: "Settings",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Hostname",
+                table: "Settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- AngularUrlShortener/Models/Settings.cs
+++ AngularUrlShortener/Models/Settings.cs
@@ -3,7 +3,7 @@
     public class Settings
     {
         public int Id { get; set; }
-        public string Hostname { get; set; }
+        public string? Hostname { get; set; }
         public bool? AllowSignup { get; set; }
         public bool? AllowAnonymousLinkCreation { get; set; }
     }
